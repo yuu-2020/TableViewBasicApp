e45e6671f75ef7ff9082f80998d270000aac1c3c
yuu-2020 <yuu.developper.2020@gmail.com>
2019-12-24 16:35:56 +0900

    Initial Commit

---
+++ TableViewBasicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewBasicApp
+//
+//  Created by Yudai Asano on 2019/12/24.
+//  Copyright © 2019 Yudai Asano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
